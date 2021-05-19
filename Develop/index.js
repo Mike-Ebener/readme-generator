660009cb96984d4c58e016e91d035aabf0584793
ME <mebener44@gmail.com>
2021-05-18 22:28:05 -0700

    updated js files

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,25 @@
 // TODO: Include packages needed for this application
 //const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
 const inquirer = require('inquirer');
-//const generatePage = require('./src/src/page-template');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = () => {
+//const questions = () => {
+
+//added code
+const promptProject = portfolioData => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+      portfolioData.projects = [];
+    }
+//end added code
     return inquirer.prompt([
       {
         type: 'input',
@@ -91,19 +105,52 @@ const questions = () => {
         message: 'Provide examples about tests and how to to run them:'
       },
 
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
   };
   
+  promptProject()
+  //.then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+    console.log(data)
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  //.then(writeFileResponse => {
+   // console.log(writeFileResponse);
+  //  return copyFile();
+  //})
+ // .then(copyFileResponse => {
+  //  console.log(copyFileResponse);
+ // })
+  .catch(err => {
+    console.log(err);
+  });
  
   
 
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
+//fs.writeFile('./readme.md', (data,err) => {
+   // if (err) throw err;
 
+
+  //  console.log('data', data);
+  //  console.log('Readme complete! Check out index.html to see the output!');
+ // });
 
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
---
+++ Develop/src/page-template.js
@@ -0,0 +1,22 @@
+
+/* # Your Project Title
+
+## Description 
+
+## Table of Contents (Optional)
+
+## Installation
+
+## Usage 
+
+## Credits
+
+## License
+
+## Badges
+
+## Features
+
+## Contributing
+
+## Tests */
